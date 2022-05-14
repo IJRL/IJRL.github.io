2faf6cad2ed1f5c9546e1c910f00c6bcea245618
Euan <33450935+euandeas@users.noreply.github.com>
2022-05-14 22:49:38 +0100

    Started major redesign

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,17 +1,29 @@
 const btn = document.querySelector(".btn-toggle");
 const icon = document.querySelector(".theme-icon");
-const linkedin = document.getElementById("linkedinicon");
-const instagram = document.getElementById("instaicon");
-const github = document.getElementById("githubicon");
+const discord = document.getElementById("discordicon");
+const twitch = document.getElementById("twitchicon");
+const youtube = document.getElementById("youtubeicon");
+const tiktok = document.getElementById("tiktokicon");
+const twitter = document.getElementById("twittericon");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
   document.body.classList.toggle("dark-theme");
   icon.src = "/assets/moon.svg";
+  discord.src = "/assets/discorddark.svg";
+  twitch.src = "/assets/twitchdark.svg";
+  youtube.src = "/assets/youtubedark.svg";
+  tiktok.src = "/assets/tiktokdark.svg";
+  twitter.src = "/assets/twitterdark.svg";
 } else if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
   icon.src = "/assets/sun.svg";
+  discord.src = "/assets/discord.svg";
+  twitch.src = "/assets/twitch.svg";
+  youtube.src = "/assets/youtube.svg";
+  tiktok.src = "/assets/tiktok.svg";
+  twitter.src = "/assets/twitter.svg";
 }
 else{
   if (!prefersDarkScheme.matches) {
@@ -28,9 +40,19 @@ else{
   localStorage.setItem("theme", theme);
   if (theme == "dark"){
     icon.src = "/assets/moon.svg";
+    discord.src = "/assets/discorddark.svg";
+    twitch.src = "/assets/twitchdark.svg";
+    youtube.src = "/assets/youtubedark.svg";
+    tiktok.src = "/assets/tiktokdark.svg";
+    twitter.src = "/assets/twitterdark.svg";
   }
   else{
     icon.src = "/assets/sun.svg";
+    discord.src = "/assets/discord.svg";
+    twitch.src = "/assets/twitch.svg";
+    youtube.src = "/assets/youtube.svg";
+    tiktok.src = "/assets/tiktok.svg";
+    twitter.src = "/assets/twitter.svg";
   }
 }
 
@@ -49,9 +71,19 @@ btn.addEventListener("click", function () {
   localStorage.setItem("theme", theme);
   if (theme == "dark"){
     icon.src = "/assets/moon.svg";
+    discord.src = "/assets/discorddark.svg";
+    twitch.src = "/assets/twitchdark.svg";
+    youtube.src = "/assets/youtubedark.svg";
+    tiktok.src = "/assets/tiktokdark.svg";
+    twitter.src = "/assets/twitterdark.svg";
   }
   else{
     icon.src = "/assets/sun.svg";
+    discord.src = "/assets/discord.svg";
+    twitch.src = "/assets/twitch.svg";
+    youtube.src = "/assets/youtube.svg";
+    tiktok.src = "/assets/tiktok.svg";
+    twitter.src = "/assets/twitter.svg";
   }
 });
 
